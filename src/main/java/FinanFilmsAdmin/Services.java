1442d81dbb807f5631e65dae0b831c966acd408c
Alipser <romarioajb@outlook.com>
2022-08-30 20:09:26 -0500

    Spring Boot implemented

---
+++ src/main/java/FinanFilmsAdmin/Services.java
@@ -0,0 +1,9 @@
+package FinanFilmsAdmin;
+
+//Imports Spring boot packages;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.List;
+@SpringBootApplication
+public class Services {
+}
